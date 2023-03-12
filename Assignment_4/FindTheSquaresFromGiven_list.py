8929cac55113fd0cdd20b22f2c78eaeac601e6b2
Aakash B. Shinde <113911171+AakashShinde123@users.noreply.github.com>
2023-03-12 20:17:39 +0530

    Add files via upload

---
+++ Assignment_4/FindTheSquaresFromGiven_list.py
@@ -0,0 +1,4 @@
+a = [4,5,2,9]
+result = map(lambda x : x*x,a)
+print("Square the elements of the list :")
+print(list(result))
---
+++ Assignment_4/FindWayWith_map.py
@@ -0,0 +1,4 @@
+a = [1, 2, 3, 4, 5, 6, 7]
+result = map (lambda x:x*3,a)
+print("tripple of list numbers :")
+print(list(result))
